505ee89a677b81537f10736f88ffc0549ab4db7a
pawel263438 <pawelapo2002@wp.pl>
2024-05-28 11:51:00 +0200

    Jest mocno, cinemabyczek i do przodu

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "cinemadb",
+    "cinemadb.apps.CinemadbConfig",
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "CEST"
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
