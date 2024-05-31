555b09e0578be540b677eacd1d181cd135243866
Mateusz Biedka <mateusz.biedka1@o2.pl>
2024-05-31 17:38:36 +0200

    Displaying videos using bootstrap and jquery

--- mysite/cinemadb/urls.py
+++ mysite/cinemadb/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("actors/<int:aktor_id>/", views.detail, name="detail"),
     path("actors/<int:aktor_id>/results/", views.results, name="results"),
     path("actors/<int:aktor_id>/vote/", views.vote, name="vote"),
+    path("videos", views.get_videos, name="get_videos"),
+    path("library", views.library, name="library"),
 ]
--- mysite/cinemadb/views.py
+++ mysite/cinemadb/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
-from .models import Aktor
+from django.http import HttpResponse, JsonResponse
+from .models import Aktor, Film
 from django.template import loader
 
 
 def index(request):
-    actors = Aktor.objects.all()
-    context = {"actors": actors}
-    return render(request, "cinemadb/index.html", context)
+    # actors = Aktor.objects.all()
+    # context = {"actors": actors}
+    return render(request, "cinemadb/index.html")
 
 
 def detail(request, aktor_id):
@@ -25,3 +25,12 @@ def results(request, aktor_id):
 
 def vote(request, aktor_id):
     return HttpResponse("You're voting on question %s." % aktor_id)
+
+
+def get_videos(request):
+    videos = list(Film.objects.values())
+    return JsonResponse(videos, safe=False)
+
+
+def library(request):
+    return render(request, "cinemadb/library.html")
